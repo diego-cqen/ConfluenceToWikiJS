078bcbc8efbbf9e64a48ae4a28a03b9961f41d9e
Diego <diego.barreto@mcn.gouv.qc.ca>
2022-07-13 10:12:45 -0400

    [edit] making the HTML convertion recursive.
    Signed-off-by: Diego <diego.barreto@mcn.gouv.qc.ca>

--- confluence2wikijs.js
+++ confluence2wikijs.js
@@ -3,30 +3,45 @@ const path = require('path');
 const fs = require('fs');
 var HTMLParser = require('node-html-parser');
 
-const directoryPath = process.argv[2];
-const outDirectoryPath = process.argv[3];
-fs.mkdirSync(outDirectoryPath)
+//const directoryPath = process.argv[2];
+//const outDirectoryPath = process.argv[3];
 
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
+convertDirectory(process.argv[2], process.argv[3]);
 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        if (file.endsWith(".html")) {
-            fs.readFile(path.join(directoryPath, file), "utf8", function(err, data) {
-                parsedHtml = HTMLParser.parse(data)
+function convertDirectory(directoryPath, outDirectoryPath) {
+    fs.mkdirSync(outDirectoryPath);
 
-                page_title = parsedHtml.querySelector("head").querySelector("title").rawText
-                page_title = page_title.substr(page_title.search(" : ") + 3)
-
-                page_data = "<!--\ntitle: " + page_title + "\n-->"
-                page_data = page_data + parsedHtml.querySelector("#main-content").innerHTML;
-
-                fs.writeFileSync(path.join(outDirectoryPath, file + ".html"), page_data)
-            });
+    fs.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
         }
+
+        //listing all files using forEach
+        files.forEach(function (file) {
+            console.log("/n/nReading: " + file);
+            if (file.endsWith(".html")) {
+                fs.readFile(path.join(directoryPath, file), "utf8", function(err, data) {
+                    parsedHtml = HTMLParser.parse(data)
+
+                    page_title = parsedHtml.querySelector("head").querySelector("title").rawText
+                    page_title = page_title.substr(page_title.search(" : ") + 3)
+
+                    page_data = "<!--\ntitle: " + page_title + "\n-->"
+                    page_data = page_data + parsedHtml.querySelector("#main-content").innerHTML;
+
+                    console.log("ConvertingHTML file to Wiki: " + file);
+                    fs.writeFileSync(path.join(outDirectoryPath, file + ".html"), page_data)
+                });
+            }
+            else if (fs.lstatSync(path.join(directoryPath, file)).isDirectory()) {
+                console.log("Scaning directory: " + file);
+                convertDirectory(path.join(directoryPath, file), path.join(outDirectoryPath, file))
+            }
+            else {
+                console.log("Copying non HTML file: " + file);
+                fs.copyFileSync(path.join(directoryPath, file), path.join(outDirectoryPath, file))
+            }
+        });
     });
-});
+}
